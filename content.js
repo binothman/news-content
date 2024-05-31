6f5877291d097561fd8cf0c1227d40fbbbb3a21d
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-05-31 16:26:49 +0300

    init

---
+++ content.js
@@ -0,0 +1,70 @@
+import puppeteer from "puppeteer";
+
+const SELECTORS = {
+  "adengad.net": ".contnews",
+  "h21.news": ".entry-content",
+  "sedda.news": ".post-text",
+  "3rd-eye.net": ".contnews",
+  "almashhadalaraby.com": ".subject_font",
+  "adnlng.info": "div[itemprop='description']",
+  "aden-tm.net": ".contnews",
+  "aden-alhadath.info": "div[itemprop='description']",
+  "manbaraden.com": "div[itemprop='description']",
+  "sabanew.net": "extract",
+  "cratersky.net": "div[itemprop='description']",
+  "shabwahalyoum.com": ".gr-element__content",
+  "tarebhtoday.online": ".entry",
+  "adenstc.com": ".gr-element__content",
+  "yeniyemen.net": ".post--content",
+  "alwfaqnews.net": "div[itemprop='articleBody']",
+  "alyompress.com": ".entry-content .news",
+  "almahrahpost.com": ".news-content",
+  "almandeb.news": ".td-post-content",
+  "aicaden.tv": ".paragraph-list",
+  "deraalganoob.com": ".dear_description",
+  "sadaalhakika.com": "div[itemprop='description']",
+  "marebpress.net": ".details",
+  "newsyemen.net": ".NewContents .mt-3.p-2",
+  "wtn-news.com": "#subjectfont",
+  "shabwaah-press.info": ".subject_font",
+  "aljnoobpost.com": ".gr-element__content",
+  "almawqeapost.net": ".description",
+  "alsahil.net": ".post_details_block",
+};
+
+export async function getContent(url) {
+  try {
+    if (!url) return null;
+
+    const link = new URL(url);
+    const host = link.hostname.replace("www.", "");
+    const selector = SELECTORS[host];
+    if (!selector) return null;
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    // Getting the element with ID 'unique-element'
+    const element = await page.$(selector);
+    let value = "";
+
+    if (element) {
+      value = await page.evaluate((el) => el.innerText, element);
+    }
+    // Performing actions on the element
+    await browser.close();
+
+    // return value.replace(new RegExp("<a.*?>", "g"), "");
+    return value
+      .replace(/(\r\n|\r|\n){2}/g, "$1")
+      .replace(/(\r\n|\r|\n){3,}/g, "$1\n")
+      .replace(/\r/g, "")
+      .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "")
+
+      .replace(/\n/g, "<br>");
+  } catch (e) {
+    return `error get the content ${e.message} ${url}`;
+  }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { getContent } from "./content.js";
+
+const app = express();
+const PORT = 3002;
+
+app.use(express.json());
+
+app.get("/post_content", async (req, res) => {
+  const { url } = req.query;
+  const content = await getContent(url);
+
+  res.status(200).send({
+    status: "success",
+    url,
+    content,
+  });
+});
+
+app.listen(PORT, () => console.log("we are live"));
