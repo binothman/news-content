cd5a33e5e75d1f8f1ea51f0c35e00dc377e55ee0
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-10-25 03:29:54 +0300

    enhancment

--- test.js
+++ test.js
@@ -1,13 +1,65 @@
 import puppeteer from "puppeteer-extra";
 import pluginStealth from "puppeteer-extra-plugin-stealth";
-import { executablePath } from "puppeteer";
+// import { executablePath } from "puppeteer";
 
 export async function getHtmlThoughCloudflare(url) {
   puppeteer.use(pluginStealth());
   const result = await puppeteer
-    .launch({ headless: true })
+    .launch({
+      defaultViewport: null,
+      headless: true,
+      ignoreHTTPSErrors: true,
+      ignoreDefaultArgs: ["--enable-automation"],
+      args: [
+        "--kiosks",
+        "--disable-accelerated-2d-canvas",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-renderer-backgrounding",
+        "--disable-canvas-aa",
+        "--disable-2d-canvas-clip-aa",
+        "--disable-gl-drawing-for-tests",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--no-zygote",
+        "--use-gl=desktop",
+        "--hide-scrollbars",
+        "--mute-audio",
+        "--no-first-run",
+        "--disable-infobars",
+        "--disable-breakpad",
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-session-crashed-bubble",
+        "--single-process",
+        "--noerrdialogs",
+        "--disabled-setupid-sandbox",
+      ],
+    })
     .then(async (browser) => {
       const page = await browser.newPage();
+
+      const client = await page.target().createCDPSession();
+      await client.send("Network.disable");
+
+      await page.setRequestInterception(true);
+
+      page.on("request", (request) => {
+        if (
+          request.resourceType() === "image" ||
+          request.resourceType() === "stylesheet" ||
+          request.resourceType() === "font" ||
+          request.resourceType() === "texttrack" ||
+          request.resourceType() === "imageset" ||
+          request.resourceType() === "bacon" ||
+          request.resourceType() === "csp_report" ||
+          request.resourceType() === "object"
+        ) {
+          request.abort();
+        } else {
+          request.continue();
+        }
+      });
+
       await page.goto(url);
       const html = await page.content();
       await browser.close();
